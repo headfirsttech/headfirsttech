680683958a60a7f9f30bf64e42d10e18f45556b3
0326 <winniringy@gmail.com>
2025-04-20 02:57:42 +0800

    chore: update packageManager configuration

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -6,8 +6,8 @@ export default defineUserConfig({
   base: "/",
 
   lang: "zh-CN",
-  title: "文档演示",
-  description: "vuepress-theme-hope 的文档演示",
+  title: "Head First Tech",
+  description: "深入浅出搞懂技术知识",
 
   theme,
 
